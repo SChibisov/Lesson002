59e08dcbabb2e7e5fc601442d7da6ccf340c84c4
SChibisov <168549074+SChibisov@users.noreply.github.com>
2024-06-04 23:25:20 +0300

    Add files via upload

---
+++ module_5_1.py
@@ -0,0 +1,17 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if 1 <= new_floor <= self.number_of_floors:
+            for i in range(1, new_floor + 1):
+                print(i)
+        else:
+            print("Такого этажа не существует")
+
+
+h1 = House('Космос 2.0', 19)
+h2 = House('ЖК Олимп', 21)
+h1.go_to(14)
+h2.go_to(22)
